e692070953c8776a77ed1131bd6680ac84bc9bda
Johannes Weber <johannes.k.weber@web.de>
2022-05-21 15:33:13 +0200

    Added intro section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { IntroComponent } from './intro/intro.component';
+import { MyWorkComponent } from './my-work/my-work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    IntroComponent,
+    MyWorkComponent
   ],
   imports: [
     BrowserModule,
