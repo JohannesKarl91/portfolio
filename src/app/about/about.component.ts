c778425867d977875bcf048ee31740fc20c76299
Johannes Weber <johannes.k.weber@web.de>
2022-05-21 22:25:08 +0200

    Added about me section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  skills = [
+    {
+      img: 'assets/img/javascript.png',
+      text: 'JavaScript'
+    },
+    {
+      img: 'assets/img/java script.png',
+      text: 'HTML'
+    },
+    {
+      img: 'assets/img/Group 205.png',
+      text: 'CSS'
+    },
+    {
+      img: 'assets/img/angular.png',
+      text: 'Angular'
+    },
+    {
+      img: 'assets/img/Git-Icon-1788C 2.png',
+      text: 'Git'
+    },
+    {
+      img: 'assets/img/icons8-rest-api-80 2.png',
+      text: 'REST API'
+    },
+    {
+      img: 'assets/img/icons8-database-52 2.png',
+      text: 'Firebase'
+    },
+    {
+      img: 'assets/img/Group 226.png',
+      text: 'Scrum'
+    },
+  ];
 
   constructor() { }
 
